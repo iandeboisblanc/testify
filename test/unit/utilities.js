a65fecd9e76a169eb6e442d2656b1a9ae648b830
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-02-19 12:31:13 -0800

    Include two tests for isValidTodoName

--- test/unit/utilities.js
+++ test/unit/utilities.js
@@ -11,4 +11,45 @@ describe('the todo.App', function() {
 });
 
 describe('the todo.util methods', function() {
+  describe('trimTodoName', function () {
+    it('should trim whitespace off input', function () {
+      //test here
+    });
+  });
+  describe('isValidTodoName', function () {
+    it('should return a boolean', function () {
+      var result = todo.util.isValidTodoName('  Get Groceries ');
+      expect(result).to.be.a('boolean');
+    });
+    it('should return false for todo names without 2 characters', function () {
+      var result = todo.util.isValidTodoName(' a  ');
+      result.should.equal(false);
+    });
+  });
+  describe('getUniqueId', function () {
+    it('should return an id value', function () {
+      //test here
+    })
+  })
 });
+
+
+// (function() {
+//   var lastId = 1;
+
+//   // Valid todo names must contain at least 2 non-space characters
+//   var validTodoRe = /[^\s]{2,}/;
+//   var whiteSpaceRe = /^\s+|\s+$/g;
+
+//   todo.util = {
+//     trimTodoName: function(name) {
+//       return name.replace(whiteSpaceRe, '');
+//     },
+//     isValidTodoName: function(name) {
+//       return validTodoRe.test(name);
+//     },
+//     getUniqueId: function() {
+//       return lastId++;
+//     }
+//   };
+// }());
