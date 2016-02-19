81228f8b801b29d8f59db91cebbfd8edcb4e78ac
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-02-19 14:46:09 -0800

    Add dependencies for sinon

--- test/integration/apiTests.js
+++ test/integration/apiTests.js
@@ -1,6 +1,17 @@
 describe('API integration', function(){
   var server, setupStub, JSONresponse;
 
+  before(function () {
+    // xhr = sinon.useFakeXMLHttpRequest();
+    // requests = [];
+    // xhr.onCreate = function (req) { requests.push(req); };
+  });
+  
+  after(function () {
+    // Like before we must clean up when tampering with globals.
+    // xhr.restore();
+  });
+
   it('todo.setup receives an array of todos when todo.init is called', function () {
   });
 });
